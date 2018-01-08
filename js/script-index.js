e13592be1b25c9690962289ace87c677837d5cf2
lpalominos <lorena.palominos@gmail.com>
2018-01-08 10:53:28 -0300

    Etapa 1: botones ok

--- js/script-index.js
+++ js/script-index.js
@@ -1,27 +1,31 @@
 $(document).ready( function(){
 
-	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
-	renderHighlightedRecipes(recipesArray);
-
+  //La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
+  renderHighlightedRecipes(recipesArray);
+  $('.js-back').hide();
+  $('.js-menu').show();
 });
 
-
-/*
-* Función que se encarga de pintar TODAS las recetas que tengan 
-* marcado el atributo "highlighted" como TRUE
-*/
-function renderHighlightedRecipes(recipesArray) {
-	console.log('Recipes: ', recipesArray);
+function printNews(){
+  $(document).append()
 }
 
 /*
-* Función que se encarga de pintar UNA recetas que tenga 
+* Función que se encarga de pintar TODAS las recetas que tengan
 * marcado el atributo "highlighted" como TRUE
-* Aqui se tiene que crear el HTML que esta en el 
+*/
+function renderHighlightedRecipes(recipesArray) {
+  console.log('Recipes: ', recipesArray);
+}
+
+/*
+* Función que se encarga de pintar UNA recetas que tenga
+* marcado el atributo "highlighted" como TRUE
+* Aqui se tiene que crear el HTML que esta en el
 * archivo "templates/templates-recipe.html"
 */
 function renderRecipe(recipe) {
-	console.log('Voy a pintar la receta: ', recipe);
+  console.log('Voy a pintar la receta: ', recipe);
 }
 
 
@@ -30,16 +34,16 @@ function renderRecipe(recipe) {
 * Función que se encarga de pintar todas las actividades
 */
 function renderActivities(activitiesArray) {
-	console.log('Activities: ', activitiesArray);
+  console.log('Activities: ', activitiesArray);
 }
 
 /*
 * Función que se encarga de pintar una actividad
-* Aqui se tiene que crear el HTML que esta en el 
+* Aqui se tiene que crear el HTML que esta en el
 * archivo "templates/templates-activity.html"
 */
 function renderActivity(recipe) {
-	
+  
 }
 
 
